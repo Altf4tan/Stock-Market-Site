d13405f62724e59b88d93adbe0fa80ac388805aa
Altan Basoglu <59366678+Altf4tan@users.noreply.github.com>
2025-05-13 00:37:07 +0300

    Update helpers.py

--- Stonks/helpers.py
+++ Stonks/helpers.py
@@ -1,14 +1,17 @@
 import requests, time
 
+from dotenv import load_dotenv
 from decimal import Decimal
 from flask import redirect, render_template, session
 from functools import wraps
 import xml.etree.ElementTree as ET
 
+load_dotenv()
+
 # Source: https://site.financialmodelingprep.com/
-FMP_KEY = "9kL9TKTMw97140buG8or4gboW2mNv3fr"
+FMP_KEY = os.environ["9kL9TKTMw97140buG8or4gboW2mNv3fr"]
 # Source https://newsapi.org
-NEWSAPI_KEY = "0260f85285a54f17a11f63a677b2f4f6"
+NEWSAPI_KEY = os.environ["0260f85285a54f17a11f63a677b2f4f6"]
 
 def apology(message, code=400):
     """Render message as an apology to user."""
